04c0f94af126816bb18ec5f510861afd586a08b4
Jay Kim <kimjingo@gmail.com>
2021-01-13 09:32:31 -0800

    create toc, content

--- src/App.js
+++ src/App.js
@@ -12,11 +12,36 @@ class Subject extends Component {
   }
 }
 
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">Javascript</a></li>
+      </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is Hyper Text Markup Language.
+      </article>
+    )
+  }
+}
+
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
